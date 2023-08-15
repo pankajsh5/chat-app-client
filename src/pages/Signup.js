971c349043f2f8f5d83c645b6d5a4885da3a62e4
pankajsh5 <120113677+pankajsh5@users.noreply.github.com>
2023-08-16 01:15:43 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -29,7 +29,7 @@ function Signup() {
     async function uploadImage() {
         const data = new FormData();
         data.append("file", image);
-        data.append("upload_preset", "your-preset-here");
+        data.append("upload_preset", `irurp0bn`);
         try {
             setUploadingImg(true);
             let res = await fetch("https://api.cloudinary.com/v1_1/dvxrvadyt/image/upload", {
